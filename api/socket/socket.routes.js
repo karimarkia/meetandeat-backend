34066f6dd089a22384a4eac27f5a821308f3e087
karim arkia <karimarkia@gmail.com>
2019-12-03 19:31:05 +0200

    changes

--- api/socket/socket.routes.js
+++ api/socket/socket.routes.js
@@ -9,16 +9,20 @@ function connectSockets(io) {
             // emits only to sockets in the same room
             // io.to(socket.myTopic).emit('chat addMsg', msg)
         })
-        socket.on('chat topic', topic=>{
-            if (socket.myTopic) {
-                socket.leave(socket.myTopic)
-            }
-            socket.join(topic)
-            socket.myTopic = topic;
-        })
-        socket.on('test event', data=>{
-            console.log(data)
-            io.emit('saying to all', data)
+        // socket.on('chat topic', topic=>{
+        //     if (socket.myTopic) {
+        //         socket.leave(socket.myTopic)
+        //     }
+        //     socket.join(topic)
+        //     socket.myTopic = topic;
+        // })
+        // socket.on('test event', data=>{
+        //     console.log(data)
+        //     io.emit('saying to all', data)
+        // })
+        socket.on('created', data=>{
+            console.log(data,'counter up')
+            io.emit('inc counter', data)
         })
 
     })
