a115d2f9342f9be2d37d898893984e804fac31f1
karim arkia <karimarkia@gmail.com>
2019-12-07 16:14:25 +0200

    chatserv

--- api/socket/socket.routes.js
+++ api/socket/socket.routes.js
@@ -4,15 +4,17 @@ module.exports = connectSockets
 function connectSockets(io) {
     io.on('connection', socket => {
         socket.on('chat newMsg', msg=>{
-            console.log(msg)
+            // console.log(msg)
             io.emit('chat addMsg', msg)
         })
-     
-      
         socket.on('created', data=>{
             // console.log(data,'counter up')
             io.emit('inc counter', data)
         })
+        socket.on('is typing', data=>{
+            io.emit('print', data)
+        })
+
 
     })
 }
