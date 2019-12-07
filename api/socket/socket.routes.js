73db07617364217e3bed73ba88fc8a374e9e1fef
karim arkia <karimarkia@gmail.com>
2019-12-07 10:50:30 +0200

    commit

--- api/socket/socket.routes.js
+++ api/socket/socket.routes.js
@@ -6,20 +6,9 @@ function connectSockets(io) {
         socket.on('chat newMsg', msg=>{
             console.log(msg)
             io.emit('chat addMsg', msg)
-            // emits only to sockets in the same room
-            // io.to(socket.myTopic).emit('chat addMsg', msg)
         })
-        // socket.on('chat topic', topic=>{
-        //     if (socket.myTopic) {
-        //         socket.leave(socket.myTopic)
-        //     }
-        //     socket.join(topic)
-        //     socket.myTopic = topic;
-        // })
-        // socket.on('test event', data=>{
-        //     console.log(data)
-        //     io.emit('saying to all', data)
-        // })
+     
+      
         socket.on('created', data=>{
             // console.log(data,'counter up')
             io.emit('inc counter', data)
